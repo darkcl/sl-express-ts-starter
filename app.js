151264a74b867209edd000788b1dbf4b1c23df92
Yeung Yiu Hung <446404+darkcl@users.noreply.github.com>
2021-05-27 15:19:31 +0800

    chore(js): remove js files

--- app.js
+++
@@ -1,6 +0,0 @@
-const { App: SuperApp } = require('@shopline/sl-express')
-
-class App extends SuperApp {}
-
-let app = new App();
-module.exports = app;
--- server.js
+++
@@ -1,8 +0,0 @@
-const app = require('./app.js');
-
-const errorHandler = function(err) {
-    console.log(err);
-    console.log('##### SERVER IS NOT STARTING CORRECTLY! #####');
-};
-
-app.start().catch(errorHandler);
