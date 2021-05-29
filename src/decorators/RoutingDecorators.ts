40f52307ec432b8bb2a74be0341803503bf406d0
Yeung Yiu Hung <446404+darkcl@users.noreply.github.com>
2021-05-29 15:54:40 +0800

    feat(http): add all http methods

---
+++ src/decorators/RoutingDecorators.ts
@@ -0,0 +1,29 @@
+import { HttpMethod } from './HttpMethod';
+
+export const routeList = [];
+
+export const controllerTable = {};
+
+export const Controller = (path = '') => {
+  return (target: Function) => {
+    controllerTable[target.name] = { basePath: path };
+  };
+};
+
+const createMethodDecorator = (method: HttpMethod = 'get') => {
+  return (path = '/'): MethodDecorator =>
+    (target: object, name: string, descriptor: any) => {
+      routeList.push({
+        type: method,
+        target: target.constructor.name,
+        name,
+        path,
+      });
+    };
+};
+
+export const Get = createMethodDecorator('get');
+export const Post = createMethodDecorator('post');
+export const Put = createMethodDecorator('put');
+export const Delete = createMethodDecorator('delete');
+export const Patch = createMethodDecorator('patch');
